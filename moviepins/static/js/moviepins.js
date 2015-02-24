6bdb8197ea855fd602fb3eef1ab47c8930216be0
Shakir Thowseen <shakirthowseen@Vanillas-iMac.local>
2015-02-24 17:42:53 -0500

    added filter template, maps pin and details template

--- moviepins/static/js/moviepins.js
+++ moviepins/static/js/moviepins.js
@@ -2,6 +2,11 @@ $(document).ready(function() {
 	$.material.init();
 	initMaps()
 
+	yearSlider()
+
+
+
+
 });
 
 
@@ -24,28 +29,67 @@ function initMaps(){
 	}
 	setGoogleMaps = function(position){
 
+		var myLatlng = new google.maps.LatLng(37.7577,-122.4376);
 
 		var mapOptions = {
-			mapTypeControl: true,
-			mapTypeControlOptions: {
-				style: google.maps.MapTypeControlStyle.DEFAULT,
-				mapTypeIds: []
-			},
+
+			panControlOptions: {
+		        // position: google.maps.ControlPosition.TOP_RIGHT
+		    },
+			mapTypeControl: false,
 			zoomControl: true,
 			zoomControlOptions: {
-				style: google.maps.ZoomControlStyle.SMALL
+				style: google.maps.ZoomControlStyle.SMALL,
+				// position: google.maps.ControlPosition.TOP_RIGHT
 			},
-
-
-			center: { lat: 37.7577, lng: -122.4376},
+			center: myLatlng,
 			// center: {lat:position.coords.latitude, lng: position.coords.longitude},
-			zoom: 12
+			zoom: 13
 
 		};
-		var map = new google.maps.Map(document.getElementById('map-canvas'),
-			mapOptions);
+		var map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
+
+		var contentString = '<div>'+
+		'<img class="cover-sm" src="images/walkingDead.jpg"><div class="moveinfo-sm">'+
+   ' <h3>Movie Title(2014)</h3>'+
+   ' <p class="text-muted">Sony Entertainment.</p>'+
+   ' <p>Jorge coloney, Emma watson</p>'+
+  '  <p>Nullam quis risus eget <a href="javascript:void(0)">urna mollis ornare</a> vel eu leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam id dolor id nibh ultricies vehicula.</p>'+
+  '</div></div>';
+
+  var infowindow = new google.maps.InfoWindow({
+      content: contentString,
+      maxWidth: 400  });
+
+
+
+		var marker = new google.maps.Marker({
+		  position: myLatlng,
+		  map: map,
+		  title: 'Hello World!'
+		});
+
+google.maps.event.addListener(marker, 'click', function() {
+    infowindow.open(map,marker);
+  });
+
+
 	}
 	getCurrentPosition()
+
+
+}
+
+function yearSlider(){
+
+		$("#yearSlider").noUiSlider({
+	start: [20, 80],
+	connect: true,
+	range: {
+		'min': 0,
+		'max': 100
+	}
+});
 
 
 }
